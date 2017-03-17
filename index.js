b22b327d996251b85a8c9bb69b1707ab09ec6292
macdja38 <jakeincanada@icloud.com>
2017-03-17 00:37:28 -0400

    Improve documentation, remove error, add warning event on recovered error.

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ Object.assign(streamResume, https);
 // let longjohn = require("longjohn");
 
 class RebindableEmitter extends EventEmitter {
+  /**
+   * Rebindable event emitter used as the base for the return object of http.get / http.request
+   * @param {EventEmitter} currentEmitter initial event emitter to bind to.
+   * @private
+   */
   constructor(currentEmitter) {
     super();
     this._onNewListener = this._onNewListener.bind(this);
@@ -27,6 +32,10 @@ class RebindableEmitter extends EventEmitter {
     this.on("removeListener", this._onRemoveListener);
   }
 
+  /**
+   * Rebinds to a new event emitter
+   * @param {EventEmitter} emitter
+   */
   bindTo(emitter) {
     let oldEmitter = this._currentEmitter;
     this._currentEmitter = emitter;
@@ -38,6 +47,12 @@ class RebindableEmitter extends EventEmitter {
     });
   }
 
+  /**
+   * Adds a filter function to an event, filter function will decide if event should be emitted or not.
+   * If the filter function returns true event is emitted anything else event will stop.
+   * @param {string} name name of event to limit
+   * @param {Function} filterFunction function to call with event.
+   */
   addFilter(name, filterFunction) {
     if (!this._filteredEvents.includes(name)) {
       this._filteredEvents.push(name);
@@ -53,6 +68,12 @@ class RebindableEmitter extends EventEmitter {
     });
   }
 
+  /**
+   * Called on a new listener being added
+   * @param {string} name
+   * @param {Function} listener
+   * @private
+   */
   _onNewListener(name, listener) {
     // console.log(listener, this._onNewListener);
     if (listener === this._onNewListener) return;
@@ -66,6 +87,12 @@ class RebindableEmitter extends EventEmitter {
     this._currentEmitter.on(name, listener);
   }
 
+  /**
+   * Called when a listener is removed
+   * @param {string} name
+   * @param {Function} listener
+   * @private
+   */
   _onRemoveListener(name, listener) {
     if (this._removingListener) return;
     if (this._activeEvents.includes(name)) {
@@ -76,17 +103,30 @@ class RebindableEmitter extends EventEmitter {
 }
 
 class ResSubstitute extends RebindableEmitter {
+  /**
+   * Extends the RebindableEvent Emitter
+   * @param {ClientRequest} res
+   * @private
+   */
   constructor(res) {
     super(res);
     this._res = res;
   }
 
+  /**
+   * Calls end on the client request
+   */
   end() {
     this._res.end();
   }
 }
 
 class OutputStream extends Readable {
+  /**
+   * Stream returned in the callback of the request
+   * @param {Object} options stream options
+   * @param {Object} httpOptions options used to make the http request
+   */
   constructor(options, httpOptions) {
     super(options);
     this._httpOptions = httpOptions;
@@ -114,11 +154,19 @@ class OutputStream extends Readable {
     this._addListeners();
   }
 
+  /**
+   * Inserts an event emitter
+   * @param {ResSubstitute} emitter
+   */
   insertEmitter(emitter) {
-    this._emiter = emitter;
-    this._emiter.addFilter("error", this._errorListener);
+    this._emitter = emitter;
+    this._emitter.addFilter("error", this._errorListener);
   }
 
+  /**
+   * Removes all the listeners from the current request
+   * @private
+   */
   _removeListeners() {
     this.res.removeListener("data", this._dataListener);
     this.res.removeListener("end", this._endListener);
@@ -128,6 +176,10 @@ class OutputStream extends Readable {
     }
   }
 
+  /**
+   * Binds the listeners to the current request
+   * @private
+   */
   _addListeners() {
     this.res.on("data", this._dataListener);
     this.res.on("end", this._endListener);
@@ -135,13 +187,13 @@ class OutputStream extends Readable {
   }
 
   /**
-   *
+   * Listens for errors and restarts the request
    * @param error
    * @returns {boolean}
    * @private
    */
   _errorListener(error) {
-    console.error("Caught", error.toString());
+    // console.error("Caught", error.toString());
     if (this.res) {
       this._removeListeners();
     }
@@ -154,6 +206,7 @@ class OutputStream extends Readable {
       this._endListener();
       return true;
     }
+    this._emitter.emit("warn", error);
     let resolveRes;
     this._resDead = new Promise((resolve, reject) => {
       resolveRes = resolve;
@@ -164,7 +217,7 @@ class OutputStream extends Readable {
     this._currentRequest = https.get(this._httpOptions,
       (res) => {
         this.res = res;
-        this._emiter.bindTo(res);
+        this._emitter.bindTo(res);
         this._addListeners();
         this._resDead = false;
         // console.log("New Res");
@@ -175,12 +228,21 @@ class OutputStream extends Readable {
     return false;
   }
 
+  /**
+   * Handles the stream ending
+   * @private
+   */
   _endListener() {
     // console.log("End fired");
     this.push(null);
     this._removeListeners();
   }
 
+  /**
+   * Handles receiving data from the stream, counting the length and buffering it.
+   * @param data
+   * @private
+   */
   _dataListener(data) {
     if (this._resDead) return;
     this._bytesSoFar += data.length;
@@ -191,6 +253,12 @@ class OutputStream extends Readable {
     }
   }
 
+  //noinspection JSUnusedGlobalSymbols
+  /**
+   * Is called by the event emitter when it needs more data, asks the current request for the data
+   * @param {number} size
+   * @private
+   */
   _read(size) {
     // console.log(size);
     if (this._resDead) {
@@ -204,6 +272,14 @@ class OutputStream extends Readable {
   }
 }
 
+/**
+ * Makes a request where econResets are handled automatically. Calls https.request internally.
+ * Similar to https.request (currently always uses https.get for subsequent requests, not recommended for use currently)
+ * @param {Object|string} options options object or a string, if a string will be parsed using url.parse()
+ * @param {number} [options.maxRetries=3] retry limit
+ * @param {function} callback
+ * @returns {ResSubstitute}
+ */
 streamResume.request = function (options, callback) {
   let requestOptions = {};
   if (typeof options === "string") {
@@ -229,6 +305,13 @@ streamResume.request = function (options, callback) {
   return emitter;
 };
 
+/**
+ * Makes a request where econResets are handled automatically. Calls https.request internally.
+ * @param {Object|string} options options object or a string, if a string will be parsed using url.parse()
+ * @param {number} [options.maxRetries=3] retry limit
+ * @param {function} callback
+ * @returns {ResSubstitute}
+ */
 streamResume.get = function (options, callback) {
   let req = streamResume.request(options, callback);
   req.end();
